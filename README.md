3c19f941209b91ec7354191e918986e1f70b6575
Dimitris-Provatas <james.provatas@gmail.com>
2020-08-27 13:14:21 +0300

    added help and invalid argument handling. added specify path function. updated md to match the current status

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # A Bash Script to import all your SQL files in your MySQL at once!
 
-This script was created for big backup projects, where you had a lot of SQL scripts created and you want to import them back to MySQL. This script will take care of all this work by simply giving it 2 arguments, a username and a password (optional), to access MySQL on your computer.
+This script was created for big backup projects, where you had a lot of SQL scripts created and you want to import them back to MySQL. This script will take care of all this work by simply giving it 2 arguments, a username, a password (optional), and a path (optional), to access MySQL on your computer.
 
-On a later itteration, it will support custom paths, but for now, you will need to run in from the folder your SQL files are located.
+It will print the time each file took, and a total time the script was running in seconds.
 
-It will print the time each file took, and a total time the script was running in seconds
+### To make the script executable, type the following command:
 
-### To run the script simply go in the folder you want it to work and type:
+<code>sudo chmod 777 /path/to/script/SQL_Import.sh</code>
 
-<code>sudo chmod 777 ./SQL_Import.sh</code><br/>
-<code>sudo ./SQL_Import.sh -u [your db username] (-p [your password])*</code>
+### To run the script type the following commad:
 
-*note that the password is optional, if the user you will use has no password, then you don't have to use the <code>-p</code> argument.
+<code>sudo /path/to/script/SQL_Import.sh -u [your db username] (-p [your password] -w [your folder path])</code>
+
+#### You can find a analytic print of the specifications with the following command:
+
+<code>/path/to/script/SQL_Import.sh -h</code>
+
+*note that the password and the path are optional. if the user you will use has no password, then you don't have to use the <code>-p</code> argument. If the script is at the same folder as the files, you do not need to specify a path, it will get currcent directory as default automaticaly.
--- SQL_Import.sh
+++ SQL_Import.sh
@@ -1,57 +1,126 @@
 #!/bin/bash
 
+# MIT License
+
+# Copyright (c) 2020 Dimitrios Provatas
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+myHelp ()
+{
+        echo "----- SQL_Import.sh Script -----"
+        echo ""
+        echo "This script was created for folders with many .sql files, e.g. server backups."
+        echo "This script will take care of all this work by simply giving it 2 arguments, a username, a password (optional), and a path (optional), to access MySQL on your computer."
+        echo "It will print the time each file took, and a total time the script was running in seconds."
+        echo ""
+        echo "Arguments:"
+        echo "  -u (Required) The username of the user you want to use. Note that the user must have all the privileges."
+        echo "  -p (Optional) The password of the user. Not required if the user has no password."
+        echo "  -w (Optional) The path to the folder containing the files. Not required if the script is in the same folder as the files"
+        echo "  -h Prints this message. Note that the -h terminates the program, so I suggest you do not use it with other arguments."
+}
+
+# When it started
 TIMESTART=$(date +%s%N)/1000000000
 
-while getopts u:p: flag
+# Get the arguments the user provided
+while getopts u:p:w:h option
 do
-        case ${flag} in
+        case ${option} in
                 u) #set usernameNAME
                         username=${OPTARG};;
                 p) #set password
                         password=${OPTARG};;
+                w) #set the path
+                        path=${OPTARG};;
+                h) #help preview
+                        myHelp
+                        exit 0;;
+                \?) #illigal options
+                        myHelp
+                        exit 128;;
         esac
 done
 
-echo "Username: $username"
-echo "Password: $password"
-
-#Loop through .sql files
-for FILE in *.sql
-do
-        # Start the timer for how long it took
-        timeItterationStart=$(date +%s%N)/1000000000
-
-        # get the file name
-        basename "$FILE"
-        fileName="$(basename -- $FILE)"
-        # echo "$fileName"
-        databaseName=${fileName%.sql}
-        # echo "$databaseName"
-
-        # Create the database if it does not exist, then
-        # Run the sql file in the mysql cli with the file name as the db
-        if [$password != ""]
+if [$username!=""]
+then
+        # Set the path
+        if [$path=""]
         then
-                mysql -u $username -p[$password] -Bse "CREATE DATABASE IF NOT EXISTS $databaseName;"
-                sleep 0.25
-                mysql -u $username -p[$password] $databaseName < ./$fileName
+                finalPath='.'
         else
-                mysql -u $username -Bse "CREATE DATABASE IF NOT EXISTS $databaseName;"
-                sleep 0.25
-                mysql -u $username $databaseName < ./$fileName
+                if [${path:-1}='/']
+                then
+                        finalPath=${path%?}
+                else
+                        finalPath=$path
+                fi
         fi
 
-        echo "done with $fileName"
+        # Print the arguments given by the user
+        echo "Username: $username"
+        echo "Password: $password"
+        echo "Folder Path: $finalPath"
 
-        # Get the time when the itteration ended
-        timeItterationStop=$(date +%s%N)/1000000000
+        # Loop through .sql files
+        for FILE in $finalPath/*.sql
+        do
+                # Start the timer for how long it took
+                timeItterationStart=$(date +%s%N)/1000000000
 
-        # Print the time elapsed
-        timeElapsed=$(($timeItterationStop-$timeItterationStart))
-        echo "Finished after: $timeElapsed seconds!"
-done
+                # get the file name
+                fileName="$(basename -- $FILE)"
+                # echo "$fileName"
+                databaseName=${fileName%.sql}
+                # echo "$databaseName"
 
-totalTimeElapsed=$(($(date +%s%N)/1000000000-$TIMESTART))
+                # Create the database if it does not exist, then
+                # Run the sql file in the mysql cli with the file name as the db
+                if [$password != ""]
+                then
+                        mysql -u $username -p[$password] -Bse "CREATE DATABASE IF NOT EXISTS $databaseName;"
+                        sleep 0.25
+                        mysql -u $username -p[$password] $databaseName < $finalPath/$fileName
+                else
+                        mysql -u $username -Bse "CREATE DATABASE IF NOT EXISTS $databaseName;"
+                        sleep 0.25
+                        mysql -u $username $databaseName < $finalPath/$fileName
+                fi
 
-echo "Done with all sql files in this folder after $totalTimeElapsed seconds."
-echo "Exiting..."
+                echo "done with $fileName"
+
+                # Get the time when the itteration ended
+                timeItterationStop=$(date +%s%N)/1000000000
+
+                # Print the time elapsed
+                timeElapsed=$(($timeItterationStop-$timeItterationStart))
+                echo "Finished after: $timeElapsed seconds!"
+        done
+
+        # Print the total time ellapsed
+        totalTimeElapsed=$(($(date +%s%N)/1000000000-$TIMESTART))
+
+        echo "Done with all sql files in this folder after $totalTimeElapsed seconds."
+        echo "Exiting..."
+else
+        # No user was given
+        echo "You need to specify at least a user with the -u argument."
+        exit 0
+fi
